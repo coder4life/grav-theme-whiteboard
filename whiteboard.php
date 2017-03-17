f2fdbe705712289f8c4465f268c97460aca374a4
coder4life <coder4life.dev@gmail.com>
2017-03-17 14:11:06 -0700

    Part 3 of GetGrav Theme - Whiteboard
    More bootstrap template fixes for missing variables in components.
    Reorganizing bootstrap structures for flexibility.
    Minor styling additions for custom partials.

--- whiteboard.php
+++ whiteboard.php
@@ -5,5 +5,14 @@ use Grav\Common\Theme;
 
 class Whiteboard extends Theme
 {
-
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onTwigExtensions' => ['onTwigExtensions', 0],
+            'onPageInitialized' => ['onPageInitialized', 0]
+        ];
+    }
 }
