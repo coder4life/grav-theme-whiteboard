2c96ce9c90388a95ec8b3ea20e2018ef46c54b8b
coder4life <coder4life.dev@gmail.com>
2017-09-23 01:20:51 -0700

    Part 6 of GetGrav Theme - Whiteboard
    Moved where CSS and Javascript are placed after compile
    Added global page template and style variables
    Added support to themes finally! Yay :tada: !
    Changed how Modular page class are working to allow individual styles
    Removed unnecessary config stubs
    Added missing Bootstrap dependency Popper.js
    Added Stackblur script to do blur effect on canvas elements and images dynamically
    Added new Pages: news, news_item, docs, doc, doc_item
    Fixed some bootstrap params for various components
    Fixed some miscellaneous HTML and CSS structuring for various extras.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const autoprefixer = require('autoprefixer'),
-  babel = require('gulp-babel'),
   cleanCSS = require('gulp-clean-css'),
   concat = require('gulp-concat'),
+  count = require('gulp-count'),
+  flatten = require('gulp-flatten'),
   flexfix = require('postcss-flexbugs-fixes'),
   gulp = require('gulp'),
-  gulpif = require('gulp-if'),
-  gutil = require('gulp-util'),
+  ifcond = require('gulp-if'),
+  util = require('gulp-util'),
   plumber = require('gulp-plumber'),
   postcss = require('gulp-postcss'),
   rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
   uglify = require('gulp-uglify'),
@@ -18,145 +20,160 @@ config = {
   js: [
     {
       name: 'bootstrap',
+      base: '.',
       in: [
-        'node_modules/bootstrap/js/src/util.js',
-        'node_modules/bootstrap/js/src/alert.js',
-        'node_modules/bootstrap/js/src/button.js',
-        //'node_modules/bootstrap/src/carousel.js',
-        'node_modules/bootstrap/js/src/collapse.js',
-        'node_modules/bootstrap/js/src/dropdown.js',
-        'node_modules/bootstrap/js/src/modal.js',
-        'node_modules/bootstrap/js/src/scrollspy.js',
-        'node_modules/bootstrap/js/src/tab.js',
-        'node_modules/bootstrap/js/src/tooltip.js',
-        'node_modules/bootstrap/js/src/popover.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
       ],
-      out: 'js-compiled',
-      compile: true,
-      minify: true,
+      out: 'js',
+      flatten: true,
     },
     {
       name: 'owlcarousel2',
+      base: '.',
       in: [
-        'node_modules/owl.carousel/src/js/owl.carousel.js',
-        'node_modules/owl.carousel/src/js/owl.animate.js',
-        'node_modules/owl.carousel/src/js/owl.autoheight.js',
-        'node_modules/owl.carousel/src/js/owl.autoplay.js',
-        'node_modules/owl.carousel/src/js/owl.autorefresh.js',
-        'node_modules/owl.carousel/src/js/owl.hash.js',
-        'node_modules/owl.carousel/src/js/owl.lazyload.js',
-        'node_modules/owl.carousel/src/js/owl.navigation.js',
-        'node_modules/owl.carousel/src/js/owl.support.js',
-        'node_modules/owl.carousel/src/js/owl.video.js'
+        'node_modules/owl.carousel/dist/owl.carousel.js',
+        'node_modules/owl.carousel/dist/owl.carousel.min.js',
       ],
-      out: 'js-compiled',
-      compile: false,
-      minify: true,
+      out: 'js',
+      flatten: true,
     },
     {
       name: 'particles',
+      base: '.',
       in: [
-        'node_modules/particles.js/particles.js'
+        'node_modules/particles.js/particles.js',
       ],
-      out: 'js-compiled',
-      compile: false,
+      out: 'js',
       minify: true,
     },
     {
-      name: 'tether',
+      name: 'popper',
+      base: '.',
       in: [
-        'node_modules/tether/src/js/utils.js',
-        'node_modules/tether/src/js/tether.js',
-        'node_modules/tether/src/js/constraint.js',
-        'node_modules/tether/src/js/abutment.js',
-        'node_modules/tether/src/js/shift.js'
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/popper.js/dist/umd/popper.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js.map',
+        'node_modules/popper.js/dist/umd/popper-utils.js',
+        'node_modules/popper.js/dist/umd/popper-utils.min.js',
+        'node_modules/popper.js/dist/umd/popper-utils.min.js.map',
       ],
-      out: 'js-compiled',
-      compile: true,
-      minify: true,
+      out: 'js',
+      flatten: true,
+    },
+    {
+      name: 'stackblur',
+      base: '.',
+      in: [
+        'node_modules/stackblur-canvas/dist/stackblur.js',
+        'node_modules/stackblur-canvas/dist/stackblur.min.js',
+      ],
+      out: 'js',
+      flatten: true,
+    },
+    {
+      name: 'tooltip',
+      base: '.',
+      in: [
+        'node_modules/tooltip.js/dist/tooltip.js',
+        'node_modules/tooltip.js/dist/tooltip.min.js',
+        'node_modules/tooltip.js/dist/tooltip.min.js.map',
+      ],
+      out: 'js',
+      flatten: true,
     },
     {
       name: 'typed',
+      base: '.',
       in: [
-        'node_modules/typed.js/js/typed.js'
+        'node_modules/typed.js/lib/typed.js',
+        'node_modules/typed.js/lib/typed.min.js',
+        'node_modules/typed.js/lib/typed.min.js.map',
       ],
-      out: 'js-compiled',
-      compile: false,
-      minify: true,
+      out: 'js',
+      flatten: true,
     }
   ],
   css: [
     { // bootstrap
       name: 'bootstrap',
+      base: '.',
       in: 'scss/vendor/bootstrap.scss',
-      out: 'css-compiled',
+      out: 'css',
+      compile: true,
+      prefix: true,
+    },
+    { // fontawesome
+      name: 'fontawesome',
+      base: '.',
+      in: 'scss/vendor/fontawesome.scss',
+      out: 'css',
       compile: true,
       prefix: true,
     },
     { // owlcarousel2
       name: 'owlcarousel2',
+      base: '.',
       in: 'scss/vendor/owlcarousel2.scss',
-      out: 'css-compiled',
+      out: 'css',
       compile: true,
       prefix: true,
     },
     { // whiteboard
       name: 'whiteboard',
+      base: '.',
       in: 'scss/whiteboard.scss',
-      out: 'css-compiled',
+      out: 'css',
       compile: true,
       prefix: true,
     }
   ],
+  fonts: [
+    {
+      name: 'fontawesome',
+      base: 'node_modules/font-awesome/fonts/',
+      in: 'node_modules/font-awesome/fonts/*',
+      out: 'fonts'
+    }
+  ],
 };
 
-gulp.task('js', function () {
-  config.js.forEach(function (js) {
-    gulp.src(js.in, {base: "."})
-      .on('end', function () {
-        gutil.log(gutil.colors.green('√'), 'Saved JS – ' + js.name);
-      })
-      .on('error', gutil.log)
-      .pipe(plumber())
-      .pipe(sourcemaps.init())
-      .pipe(gulpif(js.compile, babel())) // is the path able to compile?
-      .pipe(concat(js.name + '.js'))
-      .pipe(gulp.dest(js.out))
-      .pipe(gulpif(js.minify, rename(js.name + '.min.js')))
-      .pipe(gulpif(js.minify,
-        uglify().on('error', gutil.log))
-      ) // is the path able to minify?
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(js.out))
-  });
+supportedBrowsers = [
+  'Android 2.3',
+  'Android >= 4',
+  'Chrome >= 35',
+  'Edge >= 12',
+  'Explorer >= 10',
+  'Firefox >= 38',
+  'iOS >= 8',
+  'Opera >= 12',
+  'Safari >= 8'
+];
+
+gulp.task('clean', function () {
+  console.log("Clean all files in build folder");
+
+  return gulp.src("build/*", { read: false })
+    .pipe(clean());
 });
 
 gulp.task('css', function () {
   const streams = [];
   config.css.forEach(function (css) {
     streams.push(
-      gulp.src(css.in, {base: "."})
+      gulp.src(css.in, {base: css.base})
         .on('end', function () {
-          gutil.log(gutil.colors.green('√'), 'Saved CSS – ' + css.name);
+          util.log(util.colors.green('√'), 'Saved CSS – ' + css.name);
         })
-        .on('error', gutil.log)
+        .on('error', util.log)
         .pipe(plumber()) // make sure ready
         .pipe(sourcemaps.init())
-        .pipe(gulpif(css.compile, sass().on('error', sass.logError))) // is the path able to compile?
-        .pipe(gulpif(css.prefix,
+        .pipe(count('found ## pages', {logFiles: true}))
+        .pipe(ifcond(css.compile, sass().on('error', sass.logError))) // is the path able to compile?
+        .pipe(ifcond(css.prefix,
           postcss([
             autoprefixer({
-              browsers: [
-                'Android 2.3',
-                'Android >= 4',
-                'Chrome >= 35',
-                'Edge >= 12',
-                'Explorer >= 10',
-                'Firefox >= 38',
-                'iOS >= 8',
-                'Opera >= 12',
-                'Safari >= 8'
-              ],
+              browsers: supportedBrowsers,
               cascade: false
             }),
             flexfix
@@ -170,8 +187,39 @@ gulp.task('css', function () {
         .pipe(gulp.dest(css.out))
     );
   });
-
   return merge(streams);
+});
+
+gulp.task('fonts', function () {
+  config.fonts.forEach(function (fonts) {
+    gulp.src(fonts.in, {base: fonts.base})
+      .on('end', function () {
+        util.log(util.colors.green('√'), 'Saved Fonts – ' + fonts.name);
+      })
+      .on('error', util.log)
+      .pipe(gulp.dest(fonts.out))
+  });
+});
+
+gulp.task('js', function () {
+  config.js.forEach(function (js) {
+    return gulp
+      .src(js.in, {base: js.base})
+      .on('end', function () {
+        util.log(util.colors.green('√'), 'Saved JS – ' + js.name);
+      })
+      .on('error', util.log)
+      .pipe(plumber())
+      .pipe(ifcond(js.minify, sourcemaps.init()))
+      .pipe(ifcond(js.minify, flatten()))
+      .pipe(ifcond(js.minify, gulp.dest(js.out)))
+      .pipe(ifcond(js.minify, rename(js.name + '.min.js')))
+      .pipe(ifcond(js.minify, uglify().on('error', util.log))) // is the path able to minify?
+      .pipe(ifcond(js.minify, sourcemaps.write('.')))
+      .pipe(ifcond(js.minify, gulp.dest(js.out)))
+      .pipe(ifcond(js.flatten, flatten()))
+      .pipe(ifcond(js.flatten, gulp.dest(js.out)))
+  });
 });
 
 gulp.task('css:watch', function () {
@@ -179,4 +227,4 @@ gulp.task('css:watch', function () {
 });
 
 // Batch Gulp Tasks
-gulp.task('default', ['css', 'js']);
+gulp.task('default', ['css', 'fonts', 'js']);
